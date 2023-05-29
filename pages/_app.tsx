80732a754ce96089b95197930d87b008e39e0180
Masato Nishihara <12823962+masanishi@users.noreply.github.com>
2023-05-29 18:51:26 +0900

    更新

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import {BinanceTestnet} from "@thirdweb-dev/chains"
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = BinanceTestnet;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
